6afcbcf3b341d182a0c1d7c04ecdfb516eca1d4b
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-21 12:50:36 -0700

    completed 04

--- examples/04-typing-children/src/Application.tsx
+++ examples/04-typing-children/src/Application.tsx
@@ -1,15 +1,19 @@
 /**
  * Things you could try:
  *
- * JSX.Element;
- * JSX.Element | JSX.Element[];
- * React.ReactNode;
- * React.ReactChildren;
- * React.ReactChild[];
+ * JSX.Element; NO! - This doesn't account for arrays at all.
+ * JSX.Element | JSX.Element[]; NO! - This doesn't accept strings.
+ * React.ReactNode; YES! - Anything that works as an HTML child (Which is string, another HTML element, a react component). Accepts everything. Accepts everything 
+ * React.ReactChildren; NO! - Not at even an appropriate type; it's a utility function.
+ * React.ReactChild[]; NO! - Accepts multiple children, but it doesn't accept a single child.
  */
 
- type BoxProps = { children: any };
+ type BoxProps = { children: React.ReactNode };
 
+// Box renders children.
+// It can render more than one child.
+// That child can be another React component.
+// That child can be a standard HTML element.
  const Box = ({ children }: BoxProps) => {
    return (
      <section style={{ padding: "1em", border: "5px solid purple" }}>
