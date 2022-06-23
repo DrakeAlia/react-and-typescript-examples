de5386a2c545e3ee95ec2f03cdeccc03d8b68ab1
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-23 13:48:32 -0700

    finished 14-counter-with-state

--- examples/14-counter-for-hooks/src/Application.tsx
+++ examples/14-counter-for-hooks/src/Application.tsx
@@ -1,18 +1,42 @@
+import { ChangeEvent, useState } from 'react';
+
+const incr = (count: number) => count + 1;
+const decr = (count: number) => count - 1;
+
+// A count that were keeping in a hook
+// It know its a number and it expectes a number
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+
+  const changeCount = (event: ChangeEvent<HTMLInputElement>) => {
+    setCount(+event.target.value);
+  };
+
   return (
     <main className="Counter">
       <h1>Days Since Last Incident</h1>
-      <p className="count">0</p>
+      <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={() => setCount(incr)}>Increment</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(decr)}>Decrement</button>
       </section>
       <section className="controls">
         <form onSubmit={() => {}}>
           <label htmlFor="set-to">Set Count</label>
-          <input id="set-to" type="number" />
-          <input type="submit" />
+          <input
+            id="set-to"
+            type="number"
+            value={count}
+            onChange={changeCount}
+          />
+          <input
+            id="set-to"
+            type="number"
+            value={count}
+            onChange={(e) => setCount(+e.target.value)}
+          />
         </form>
       </section>
     </main>
