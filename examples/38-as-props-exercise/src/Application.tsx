99e4fc00bfa5c08deaa80602379608aa98a93dee
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-25 15:45:25 -0700

    finished 38-as-props-exercise

--- examples/38-as-props-exercise/src/Application.tsx
+++ examples/38-as-props-exercise/src/Application.tsx
@@ -1,20 +1,40 @@
 import * as React from 'react';
 
-type TextProps = {
+// Polymorphic Components (Exercise)
+
+type TextOwnProps<E extends React.ElementType = React.ElementType> = {
   children: string;
+  as?: E;
 };
+
+// And we want out custom component to proxy all of its props to the HTML
+// Element minus our special sauce.
+
+// We're using Omit as a helper in this case.
+type TextProps<E extends React.ElementType> = TextOwnProps<E> &
+  Omit<React.ComponentProps<E>, keyof TextOwnProps>;
 
 const exampleText =
   'When I was born, the name for what I was did not exist. They called me nymph, assuming I would be like my mother and aunts and thousand cousins. Least of the lesser goddesses, our powers were so modest they could scarcely ensure our eternities. We spoke to fish and nurtured flowers, coaxed drops from the clouds or salt from the waves. That word, nymph, paced out the length and breadth of our futures.';
 
-function Text({ children }: TextProps) {
-  return <div>{children}</div>;
+const defaultElement = 'div';
+
+// Finally, we'll refactor our Text component as follows:
+function Text<E extends React.ElementType = typeof defaultElement>({
+  children,
+  as,
+  ...rest
+}: TextProps<E>) {
+  const TagName = as || defaultElement;
+  return <TagName {...rest}>{children}</TagName>;
 }
 
 const Application = () => {
   return (
     <main>
-      <Text>{exampleText}</Text>
+      <Text id="main" as="label" htmlFor="wow">
+        {exampleText}
+      </Text>
     </main>
   );
 };
