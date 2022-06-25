82780c00a6e4825221c2298d2756bcf64c90dd2c
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-25 12:24:28 -0700

    finished part of 27-charactert-card-base-for-utility-types

--- examples/27-character-card-base-for-utility-types/src/Table.tsx
+++ examples/27-character-card-base-for-utility-types/src/Table.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import { CharacterType } from './characters';
 
+// We can do a quick refactor make our TableRow components 
+// better.
+
+// We could handle this a few different ways:
+// 1.Capitalize the heading with JavaScript.
+// 2.Capitalize the heading with CSS.
+// 3.Fix the typing.
+
+
+// It would be cool if I could enforce that the heading 
+// be property that is actually on a character.
+
 type TableProps = { children: React.ReactNode };
+// We can even dynamically type the value to be any
+// reasonable type found in the values of CharacterType.
 type TableRowProps = {
-  heading: string;
-  value: string | number;
+  heading: Capitalize<keyof CharacterType>;
+  value: CharacterType[keyof CharacterType];
 };
 
 export const Table = ({ children }: TableProps) => {
@@ -23,3 +37,21 @@ export const TableRow = ({ heading, value }: TableRowProps) => {
     </tr>
   );
 };
+
+
+// Yes. I could hardcode in the values, but I don't love 
+// that.
+
+// type TableRowProps = {
+//   heading:
+//     | 'Name'
+//     | 'Alignment'
+//     | 'Intelligence'
+//     | 'Strength'
+//     | 'Speed'
+//     | 'Durability'
+//     | 'Power'
+//     | 'Combat'
+//     | 'Total';
+//   value: string | number;
+// };
