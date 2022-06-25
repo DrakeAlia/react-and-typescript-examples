3fd10a89d603834ff8d02f2e1c5a31082e808ded
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-25 13:38:56 -0700

    added notes in 32

--- examples/32-character-card-with-hoc/src/Application.tsx
+++ examples/32-character-card-with-hoc/src/Application.tsx
@@ -3,6 +3,13 @@ import { CharacterInformation } from './CharacterInformation';
 import { CharacterType, fetchCharacter } from './characters';
 import { Loading } from './Loading';
 
+// We'll start at a super high level with just a
+// component that wraps another component and does
+// nothing to it.
+
+// Okay, so what does our HOC pass in? Well, in this case,
+// it passes in character. Right on. Let's get clear
+// about that.
 type WithCharacterProps = {
   character: CharacterType;
 };
@@ -10,6 +17,10 @@ type WithCharacterProps = {
 function withCharacter<T extends WithCharacterProps>(
   Component: React.ComponentType<T>
 ) {
+  // Now, we'll tell our HOC, that cool—set the generic,
+  // T to the type of the component that we pass in,
+  // but that component must have a character prop that
+  // is of the type CharacterType.
   return (props: Omit<T, keyof WithCharacterProps>) => {
     const [character, setCharacter] = React.useState<CharacterType | null>(
       null
@@ -23,12 +34,20 @@ function withCharacter<T extends WithCharacterProps>(
       });
     }, []);
 
+    // We still have the issue with the fact that it will
+    // take any props, however.
+    // What we want is for our higher order component to
+    // take any of the props the component it wraps takes,
+    // except the ones we plan on passing in.
     if (loading) return <Loading />;
     return character && <Component {...(props as T)} character={character} />;
   };
 }
 
 const Application = () => {
+  // In the Application component, we can use our pointless HOC.
+  // Just simulating that is the combination of our character information and 
+  // component with character higher-order component as well 
   const CharacterInformationWithCharacter = withCharacter(CharacterInformation);
 
   return (
